069d56653fa3af492e255c50b44b0b08e957ad72
Jun C <kjstudios858@gmail.com>
2018-08-06 00:26:43 -0700

    refactor Added new function to create a new type of post type called event

--- functions.php
+++ functions.php
@@ -18,7 +18,13 @@
 
     add_action('after_setup_theme', 'university_features');
 
+    function university_post_types() {
+        register_post_type('event', array(
+            'public' => true
+        ));
+    }
 
+    add_action('init', 'university_post_types');
 
 
 ?>
