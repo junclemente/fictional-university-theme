cd6352310d097f50d3dded52252e8a3e4bb94cc9
Jun C <kjstudios858@gmail.com>
2018-08-06 08:28:52 -0700

    refactor Update code

--- archive-event.php
+++ archive-event.php
@@ -22,17 +22,17 @@ This is the template page for archive-event display pages.
 
   while(have_posts()) {
     the_post(); ?>
-    <div class="post-item">
-      <h2 class="headline headline--medium headling--post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <div class="metabox">
-        <p>Post by <?php the_author_posts_link(); ?> on <?php the_time('n.j.y'); ?> in <?php echo get_the_category_list(', ') ?></p>
-      </div>
-      <div class="generic-content">
-        <?php the_excerpt(); ?>
-        <p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading...</a></p>
+
+    <div class="event-summary">
+      <a class="event-summary__date t-center" href="#">
+        <span class="event-summary__month">Mar</span>
+        <span class="event-summary__day">25</span>
+      </a>
+      <div class="event-summary__content">
+        <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+        <p><?php echo wp_trim_words(get_the_content(), 18); ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
       </div>
     </div>
-
   <?php }
 
   echo paginate_links();
