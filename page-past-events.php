e422eb242149069257751c51364ab246d1023cdb
Jun C <kjstudios858@gmail.com>
2018-08-07 16:33:28 -0700

    refactor Create custom query to select past-events

--- page-past-events.php
+++ page-past-events.php
@@ -20,22 +20,22 @@ This is the template page for past-event display pages.
 <?php
 
     $today = date('Ymd');
-      $pastEvents = new WP_Query(array(
-        'paged' => get_query_var('paged', 1),
-        // 'posts_per_page' => 1,
-        'post_type' => 'event',
-        'meta_key' => 'event_date',
-        'orderby' => 'meta_value_num',
-        'order' => 'ASC',
-        'meta_query' => array(
-          array(
-            'key' => 'event_date',
-            'compare' => '<',
-            'value' => $today,
-            'type' => 'numeric'
-          )
+    $pastEvents = new WP_Query(array(
+      'paged' => get_query_var('paged', 1),
+      // 'posts_per_page' => 1,
+      'post_type' => 'event',
+      'meta_key' => 'event_date',
+      'orderby' => 'meta_value_num',
+      'order' => 'ASC',
+      'meta_query' => array(
+        array(
+          'key' => 'event_date',
+          'compare' => '<',
+          'value' => $today,
+          'type' => 'numeric'
         )
-      ));
+      )
+    ));
 
   while($pastEvents->have_posts()) {
     $pastEvents->the_post(); ?>
